9bcb65ede1d9a96d228468888822cdf2be8794fa
mexcaptain <mexcaptain@gmail.com>
2019-01-20 14:31:50 +0300

    Index-page. Slider

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,32 @@
-$(document).ready(function () {
-    svg4everybody({});
+$(function () {
+    indexSlider();
 });
+
+function indexSlider() {
+    var slider = $('.js-index-slider');
+
+    if (slider.length) {
+        slider.slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoPlay: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 481,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+            ],
+
+
+        });
+    }
+}

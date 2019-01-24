860bd1cd786ce12151f58d8bebf07d06201262ac
mexcaptain <mexcaptain@gmail.com>
2019-01-25 02:53:02 +0300

    Index-page. Links slider

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -7,7 +7,10 @@ function indexSliders() {
         sliderMain = $('.js-index-slider-main'),
         sliderMainPrev = $('.js-index-slider-main-prev'),
         sliderMainNext = $('.js-index-slider-main-next'),
-        sliderFest = $('.js-index-slider-fest');
+        sliderFest = $('.js-index-slider-fest'),
+        sliderLinks = $('.js-index-slider-links'),
+        sliderLinksPrev = $('.js-index-slider-links-prev'),
+        sliderLinksNext = $('.js-index-slider-links-next');
 
     if (sliderTop.length) {
         sliderTop.slick({
@@ -59,4 +62,54 @@ function indexSliders() {
             dots: true
         });
     }
+
+    if (sliderLinks.length) {
+        sliderLinks.slick({
+            arrows: true,
+            adaptiveHeight: true,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            autoPlay: true,
+            responsive: [
+                {
+                    breakpoint: 1281,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 661,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 381,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+            ],
+        });
+    }
+
+    sliderLinksPrev.on('click', function (e) {
+        e.preventDefault();
+        sliderLinks.slick('slickPrev');
+    });
+
+    sliderLinksNext.on('click', function (e) {
+        e.preventDefault();
+        sliderLinks.slick('slickNext');
+    });
 }
